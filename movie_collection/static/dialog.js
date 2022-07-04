a279c98d33c61477220d4eb191462ee4391ed413
Geoff Beier <geoff@tuxpup.com>
2022-07-04 16:40:25 -0400

    use tailwind to style templates, tailwind + vanilla js to show/hide modal

--- movie_collection/static/dialog.js
+++ movie_collection/static/dialog.js
@@ -1,23 +1,26 @@
 ;(function () {
-  const modal = new bootstrap.Modal(document.getElementById("modal"))
+  const modal = document.getElementById("modal");
 
   htmx.on("htmx:afterSwap", (e) => {
     // Response targeting #dialog => show the modal
     if (e.detail.target.id == "dialog") {
-      modal.show()
+      modal.classList.toggle('hidden')
     }
   })
 
   htmx.on("htmx:beforeSwap", (e) => {
     // Empty response targeting #dialog => hide the modal
     if (e.detail.target.id == "dialog" && !e.detail.xhr.response) {
-      modal.hide()
+      modal.classList.toggle('hidden')
       e.detail.shouldSwap = false
     }
   })
 
-  // Remove dialog content after hiding
-  htmx.on("hidden.bs.modal", () => {
-    document.getElementById("dialog").innerHTML = ""
+  modal.addEventListener("dismissed", () => {
+    document.getElementById("dialog").innerHTML = "";
   })
+  // Remove dialog content after hiding
+  //htmx.on("transitioned", () => {
+    //document.getElementById("dialog").innerHTML = ""
+  //})
 })()
