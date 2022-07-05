ed5eaf4fc79d4b3cdc918b365f5c99c360e35a9d
Geoff Beier <geoff@tuxpup.com>
2022-07-05 00:14:37 -0400

    Use alpine.js + tailwind instead of bootstrap for toasts

--- movie_collection/static/toast.js
+++ movie_collection/static/toast.js
@@ -1,10 +1,38 @@
-;(function () {
-  const toastElement = document.getElementById("toast")
-  const toastBody = document.getElementById("toast-body")
-  const toast = new bootstrap.Toast(toastElement, { delay: 2000 })
+function toastHandler() {
+  return {
+    toasts: [],
+    visible: [],
+    add(toast) {
+      toast.id = Date.now()
+      this.toasts.push(toast)
+      this.fire(toast.id)
+    },
+    fire(id) {
+      this.visible.push(this.toasts.find(toast => toast.id == id))
+      const timeShown = 2000 * this.visible.length
+      setTimeout(() => {
+        this.remove(id)
+      }, timeShown)
+    },
+    remove(id) {
+      const toast = this.visible.find(toast => toast.id == id)
+      const index = this.visible.indexOf(toast)
+      this.visible.splice(index, 1)
+    },
+  }
+}
 
+;(function () {
   htmx.on("showMessage", (e) => {
-    toastBody.innerText = e.detail.value
-    toast.show()
+    let message_detail = {type: 'info', text: '🎬 ' + e.detail.value}
+    dispatchEvent(
+        new CustomEvent('notice', {
+              detail: message_detail,
+              bubbles: true,
+              cancelable: true,
+              composed: true
+            }
+        )
+    );
   })
 })()
